10deb1c87963ded0b79b146e0d2faae51cecf9a7
Phi Nguyen <phi.nguyen@infonam.com>
2014-12-14 00:49:19 +0700

    - Added: Help View Controller

---
+++ Pokedex Characters/Pokedex Characters/HelpViewController.h
@@ -0,0 +1,13 @@
+//
+//  HelpViewController.h
+//  Origami Paper Art
+//
+//  Created by Phi Nguyen on 12/14/14.
+//  Copyright (c) 2014 Duc Thien. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelpViewController : UIViewController
+
+@end
